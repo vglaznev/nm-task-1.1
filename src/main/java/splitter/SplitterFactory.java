afefc57797cbc44f2af769fdeef83568a97d08a7
Vsevolod Glaznev <vsevolod.glaznev@gmail.com>
2022-09-01 00:14:48 +0300

    remove config

--- src/main/java/splitter/SplitterFactory.java
+++ src/main/java/splitter/SplitterFactory.java
@@ -1,17 +1,10 @@
 package splitter;
 
-import config.Config;
-
 import java.util.Set;
 
 public class SplitterFactory {
-    private static Set<String> chebyshevSplitterIdentifiers;
-    private static Set<String> uniformSplitterIdentifiers;
-
-    static {
-        chebyshevSplitterIdentifiers = Set.of(Config.getProperty("splitter.chebyshev.name").split("#"));
-        uniformSplitterIdentifiers = Set.of(Config.getProperty("splitter.uniform.name").split("#"));
-    }
+    private static Set<String> chebyshevSplitterIdentifiers = Set.of("chebyshev", "чебышев", "чебышёв");
+    private static Set<String> uniformSplitterIdentifiers = Set.of("uniform", "равномерное");
 
     public static IntervalSplitter getSplitter(String splitterType) {
         String splitterTypeLower = splitterType.toLowerCase();
